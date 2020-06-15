6f4762a45e7f278d6e462bba8190ee9fc0b68971
Iacopo di Gregorio <iacopodigregorio@ip-172-16-240-62.eu-west-1.compute.internal>
2020-06-15 16:20:31 +0200

    done 1/2 steps for for loop to generate automatic xpath according to how many keywords

--- spider_jobindex.py
+++ spider_jobindex.py
@@ -3,15 +3,38 @@ import scrapy
 class JobindexSpider(scrapy.Spider):
     name = 'jobindex'
     start_urls = [
-        'https://www.jobindex.dk/jobsoegning/it/virksomhedssystemer/storkoebenhavn'
+        'https://www.nets.eu/careers/Pages/Job-openings.aspx?jobId=17562',
     ]
+# 1 create an iterable object which generates further links (next page):
 
-    def parse(self, response):
-        page = response.url.split("/")[-1]
-        filename = 'joblist_page-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+
+# 2 create an iterable object which stores:
+# - the links to the job ads
+# - the job title
+# - the uploaded for the job ads
+# - detects the language 
 
 ## Xpath to select Apply buttons links
-## //a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),"apply")] | //button[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),"apply")] 
+# - apply url
+# - the job title
+    def parse(self, response):
+        #get combination of xpaths for apply links likely to happen within the nodes and keywords
+        nodes = ["a", "href"]
+        key_words = ["apply", "interest"]
+        xpath_base = '//{}[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),"{}")]'
+        total_combination = len(nodes) * len(key_words)
+        xpath_query = ''
+        for i in range(total_combination):
+            xpath_query = xpath_base.format(nodes[i % len(nodes)], key_words[i // 2])
+
+
+        xpath_query = xpath_base.format(nodes[0], key_words[0])
+        xpath_query
+        for node in response.xpath(''):
+            yield {
+
+            }
+
+# response.xpath('//a[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),"apply")] | //button[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),"apply")]').get()
+
+
